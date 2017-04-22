e28e97ca23bd4452592a96677b743fd304c8f863
Ryan MacGillivray <rmacg89@gmail.com>
2017-04-22 21:34:22 +0100

    Initial commit

---
+++ lib/grammer.ex
@@ -0,0 +1,18 @@
+defmodule Grammer do
+  @moduledoc """
+  Documentation for Grammer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Grammer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/grammer_test.exs
@@ -0,0 +1,8 @@
+defmodule GrammerTest do
+  use ExUnit.Case
+  doctest Grammer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
